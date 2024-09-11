674c95b661d7ea6ee08588b105b1a5f6eba44d10
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2024-09-11 09:56:41 +0900

    [level 1] Title: 콜라 문제, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,13 @@
+function solution(a, b, n) {
+    let totalCola = 0
+while(n>=a){
+    const newCola = Math.floor(n/a)*b
+    totalCola +=newCola
+  n=(n%a)+newCola
+} return totalCola
+
+}
+     
+   
+    
+ 
