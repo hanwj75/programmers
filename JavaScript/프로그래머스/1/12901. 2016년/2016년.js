90a8152c09fcf6ec59ab678c34e54df3da7e60f4
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2024-09-13 09:39:22 +0900

    [level 1] Title: 2016년, Time: 0.15 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    const week = ['SUN','MON','TUE','WED','THU','FRI','SAT']
+    const dayOfWeek = new Date(`2016-${a}-${b}`).getDay(); 
+
+for(let i=0; i<week.length;i++){
+   
+  if(dayOfWeek === i){
+      week[i]
+      let result = week[i]
+     
+      return result
+      
+  } 
+}
+}
