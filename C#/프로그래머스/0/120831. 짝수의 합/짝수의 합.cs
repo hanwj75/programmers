9aafb2aa5a51dc91522ab6690232ae9642e08729
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-01-17 17:19:46 +0900

    [level 0] Title: 짝수의 합, Time: 0.26 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int result = 0;
+        for(int i=2; i<=n;i+=2){           
+                result +=i;     
+        }
+        return result;
+    }
+}
