d2f6ecc8ef7d71cf347e84a647e1635013e0f238
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-02-20 18:44:03 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.48 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ JavaScript/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,5 +1,19 @@
 function solution(s) {
-   const arr =Array.from(s)
-   const res =arr.sort().reverse().join('')
-   return res
+const lower = [];
+const upper= [];
+
+ for(let i=0;i<s.length;i++){
+   if(!/[A-Z]/g.test(s[i])){
+       lower.push(s[i])
+   }else{
+       upper.push(s[i])
+   }
+     
+ }
+    lower.sort().reverse()
+    upper.sort().reverse()
+    return lower.join('')+upper.join('')
 }
+
+// 대문자를 찾는 패턴 : /[A-Z]/g 
+// 대문자가 아닌 경우 : !/[A-Z]/g.test()
