07358607e751fff2cd6dcddcca84e1cff6d6fba0
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-02-24 19:01:03 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ JavaScript/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,5 @@
 function solution(seoul) {
-for(let i = 0; i<=seoul.length;i++){
-    if(seoul[i]==='Kim'){
-        return `김서방은 ${i}에 있다`
-    }
-}
+    const x = seoul.indexOf('Kim')
+    return `김서방은 ${x}에 있다`
+
 }
