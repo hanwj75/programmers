7d657a9a9a909c93cb255898a387612579fddccc
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-02-24 17:03:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ JavaScript/프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,17 +1,14 @@
 function solution(x) {
-let length = x.toString()
-let arr = Array.from(length).map(Number)
-let sum = 0
-for(let i = 0 ; i<arr.length;i++){
- sum+=arr[i]
+    let sum = 0
+    let digits = x.toString().split('')
+for(let i=0;i<digits.length;i++){
+    sum+=Number(digits[i])
+
 }
-   if(x%sum === 0){
-       return true
-   }else{
-       return false
-   }
-
-    
+     
+    if(x%sum===0){       
+        return true
+    }else{
+        return false
+    }
 }
-
-
