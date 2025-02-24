dfcffcd83dabbe56111dbed7228085598b2cc4cc
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-02-24 18:47:31 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ JavaScript/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,20 +1,24 @@
 function solution(num) {
-    let count = 0
-   while(num!==1){
-       if(num%2===0){
-           num = num/2
-           count++
-       }else if(num%2!==0){
-           num= num*3+1
-           count++
-       }else if(num===1){
-           return 0
-       }
-      
-   } 
-    if(count>=500){
-        return -1
-    }
-    
-    return count
+let limit = 500
+
+
+if(num === 1){
+    return 0
 }
+
+for(let i=1;i<=limit;i++){
+if( num !== 1){
+    if(num%2===0){
+        num = num/2
+    }else{
+        num = num*3 +1
+    }
+   if(num === 1){
+       return i
+   }else if(i>=limit){
+       return -1
+   }
+}
+
+}
+   }
