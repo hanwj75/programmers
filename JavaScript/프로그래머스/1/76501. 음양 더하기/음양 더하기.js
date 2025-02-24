71e50fb181aa8a501d096b79edcb7c2a9e94f0d8
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-02-25 01:50:04 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ JavaScript/프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,12 @@
 function solution(absolutes, signs) {
-  let num = 0
-  for(let i = 0 ; i<absolutes.length;i++){
-      if(signs[i]===false){
-          num-=absolutes[i]
-          console.log(num)
-      }else{
-          num+=absolutes[i]
-      }
-  }return num
+    let result = 0
+  
+ for(let i = 0 ; i<absolutes.length;i++){
+ 
+     if(signs[i]===true){
+         result+=absolutes[i]
+     }else{
+         result-=absolutes[i]
+     }
+ }return result
 }
