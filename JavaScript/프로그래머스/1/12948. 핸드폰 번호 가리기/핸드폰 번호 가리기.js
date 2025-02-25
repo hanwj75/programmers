57ac21f0c04e62a2fee6748c1a643333dae97b8a
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-02-25 15:48:42 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,10 +1,10 @@
 function solution(phone_number) {
- let secret_number = []
- for(let i = 0 ; i<phone_number.length;i++){
-     if(i<phone_number.length-4){
-         secret_number.push("*")
-     }else{
-         secret_number.push(phone_number[i])
-     }
- }return secret_number.join('')
+    let starNum = []
+for(let i=0 ; i<=phone_number.length;i++){
+    if(i<phone_number.length-4){
+        starNum.push('*')
+    }else{
+        starNum.push(phone_number[i])
+    }
+}return starNum.join('')
 }
