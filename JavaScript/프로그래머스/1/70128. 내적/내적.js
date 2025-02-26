37a0e8122599c7a57dc08c6d871b575f0d0dd04c
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-02-26 15:32:51 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/70128. 내적/내적.js
+++ JavaScript/프로그래머스/1/70128. 내적/내적.js
@@ -1,11 +1,8 @@
 function solution(a, b) {
-    let len = Math.max(a.length,b.length)
 let sum = 0
-      for(let i=0;i<len;i++){
-          if(i<len){
-              sum+=a[i]*b[i]
-         
-          }
-      }return sum
-  
+for(let i = 0 ; i<a.length;i++){
+    
+    sum+=a[i]*b[i]
+}
+    return sum
 }
