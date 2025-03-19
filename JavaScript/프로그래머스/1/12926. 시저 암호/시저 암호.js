904a1792ee96f56e39754e3ebdff9d3dd98017b6
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-03-19 19:18:49 +0900

    [level 1] Title: 시저 암호, Time: 5.10 ms, Memory: 37.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ JavaScript/프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,26 +1,26 @@
 function solution(s, n) {
-    let result = ''
-    for(let i = 0 ; i<s.length;i++){
-        let char = s[i]
-        let basic = 0
-         let charCode = char.charCodeAt(0) 
-         if(char === ' '){
-             result += " "
-         }
-        else if(char >= 'a' &&char <='z'){
-            let basic = 'a'.charCodeAt(0)
-           charCode = ((charCode-basic+n)%26) + basic
-            
-        result+=String.fromCharCode(charCode)
-            
-        }
-         else if(char >= 'A' &&char <='Z'){
-            let basic = 'A'.charCodeAt(0)
-           charCode = ((charCode-basic+n)%26) + basic
-            
-        result+=String.fromCharCode(charCode)
-            
-    }  
+let result = ""
 
-}return result
+    for(let i = 0 ; i<s.length;i++){
+        let rawCode =  s[i].charCodeAt()
+        //소문자의 경우 a~z = 97~122
+        if(rawCode >=97&&rawCode<=122){
+        
+        let lowerIndexCode = (rawCode - 97+n)%26+97
+        result+=String.fromCharCode(lowerIndexCode)
+    }
+        //대문자의 경우 A~Z = 65~90
+        else if(rawCode>=65&&rawCode<=90){
+            
+        let upperIndexCode = (rawCode - 65+n)%26+65
+         result+=String.fromCharCode(upperIndexCode)
+        }else{
+            //97~122 , 65~90 이 아닐경우 기존 값을 그대로 추가
+            result+=s[i]
+        }
+      
+ 
+    }return result
 }
+
+//65 = A , z = 122 , Z = 90 , a = 97
