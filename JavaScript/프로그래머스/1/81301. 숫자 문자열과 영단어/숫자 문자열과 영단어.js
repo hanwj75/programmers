bc7661967ecfde3d4db906361c58bac3b996ea04
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2025-03-20 20:51:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ JavaScript/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,9 +1,9 @@
 function solution(s) {
-   let abc = ['zero','one','two','three','four','five','six','seven','eight','nine']
-  let   result =s
-for(let i = 0 ; i<abc.length;i++){
-   let arr =result.split(abc[i])
-   result=arr.join(i)
-   
-}return Number(result)
-   }
+let numbers = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+
+for(let i = 0 ; i<numbers.length;i++){  
+ let str = s.split(numbers[i])
+ s = str.join(i)
+}
+    return Number(s)
+}
