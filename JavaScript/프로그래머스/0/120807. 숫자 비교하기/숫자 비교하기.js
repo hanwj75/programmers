ec5a102e81115e3f9ef1805e4014b82ae95a4043
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2026-04-11 01:15:49 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ JavaScript/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,5 +1,10 @@
 function solution(num1, num2) {
-  if(num1===num2){
-      return 1
-  }else return -1
+    let res 
+    if(num1===num2){
+     res = 1
+    }else{
+     res = -1
+    }
+  
+    return res;
 }
