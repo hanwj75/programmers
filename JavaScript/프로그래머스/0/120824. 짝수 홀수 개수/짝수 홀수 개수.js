b21f8a9b02bfe9e484be31d42a25a901ef5e71e6
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2026-04-11 18:41:55 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    let even = 0
+    let odd = 0
+    let arr = []
+    for(let i=0; i<num_list.length;i++){
+        if(num_list[i]%2===0){
+            even++
+        }else{
+            odd++
+        }
+    }
+    arr.push(even,odd)
+    
+    return arr
+}
