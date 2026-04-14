a199a34208f53abf24b0171212148706cb045426
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2026-04-14 23:53:08 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -0,0 +1,13 @@
+function solution(box, n) {
+
+   
+    let box1 =Math.trunc(box[0]/n)
+    let box2 =Math.trunc(box[1]/n)
+    let box3 =Math.trunc(box[2]/n)
+    let res = box1*box2*box3
+  return res
+   }
+
+
+
+
